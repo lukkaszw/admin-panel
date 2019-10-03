7b3e4f62e940056df3a3159392bc5c3b1daf0bd7
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2019-10-03 22:24:22 +0200

    Update Sidebar.toggleSideBarActivity method

--- docs/js/components/SideBar.js
+++ docs/js/components/SideBar.js
@@ -13,14 +13,15 @@ class SideBar {
     this.dom.burgerBtnElement = this.dom.wrapper.querySelector(select.sideBar.burgerBtn);
     this.burger = new MenuBurgerBtn(this.dom.burgerBtnElement);
 
-    this.dom.wrapper.addEventListener('burgerClicked', (e) => {
-      const isActive = e.detail.isBtnActive;
-      this.toggleSideBarActivity(isActive);
+    this.dom.wrapper.addEventListener('burgerClicked', () => {
+      // const isActive = e.detail.isBtnActive;
+      this.toggleSideBarActivity();
     });
   }
 
-  toggleSideBarActivity(isActive) {
-    isActive ? this.dom.sideBarElement.classList.add('active') : this.dom.sideBarElement.classList.remove('active');
+  toggleSideBarActivity() {
+    // isActive ? this.dom.sideBarElement.classList.add('active') : this.dom.sideBarElement.classList.remove('active');
+    this.dom.sideBarElement.classList.toggle('active');
   }
 
 
